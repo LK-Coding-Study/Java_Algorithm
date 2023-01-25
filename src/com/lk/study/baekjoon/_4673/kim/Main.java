d9c18e7e7631969fe40e8ceac0838db7d1c0ba66
idean3885 <dykimdev3885@gmail.com>
2023-01-25 22:08:31 +0900

    feat/4673 셀프넘버 브랜치 추가
    브랜치 및 기본 docs 추가

--- src/com/lk/study/baekjoon/_4673/kim/Main.java
+++ src/com/lk/study/baekjoon/_4673/kim/Main.java
@@ -1,4 +1,12 @@
 package com.lk.study.baekjoon._4673.kim;
 
+/**
+ * <h3>브루트포스 > 4673 셀프넘버</h3>
+ *
+ * <a href="https://www.acmicpc.net/problem/4673">문제 링크</a>
+ */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("브랜치 추가");
+    }
 }
