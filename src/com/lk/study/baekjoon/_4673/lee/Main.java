e5973f80d2ba9e8b5d5fd759dba04e4d6ef0ef24
ehdgjslee <wkdak2164@gmail.com>
2023-02-01 01:56:47 +0900

    feat/ 4673번 문제 추가
    브렌치 및 메인코드 추가

--- src/com/lk/study/baekjoon/_4673/lee/Main.java
+++ src/com/lk/study/baekjoon/_4673/lee/Main.java
@@ -1,7 +1,30 @@
 package com.lk.study.baekjoon._4673.lee;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        List a = new ArrayList<>();
+
+        int sum = 0;
+        int n = 0;
+
+        for (int i = 1; i<=10000; i++){
+            sum = i;
+            n = i;
+            while (n >= 1) {
+                sum += n % 10;
+                n /= 10;
+            }
+            a.add(sum);
+        }
+
+        for(int j = 1; j<=10000; j++){
+            if(!a.contains(j)){
+                System.out.println(j);
+            }
+        }
+
     }
 }
